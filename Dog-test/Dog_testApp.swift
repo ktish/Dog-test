75b99919383ffdd96a0b587acc588818030600a9
Katterin Soto <sotokatterin@gmail.com>
2021-03-03 11:42:53 -0500

    Initial Commit

---
+++ Dog-test/Dog_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_testApp.swift
+//  Dog-test
+//
+//  Created by Katterin Soto on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Dog_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
